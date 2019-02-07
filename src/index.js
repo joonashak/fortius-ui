986ab77c97d57bd97d16c90f470e2ed64a8b0e96
Joonas Häkkinen <joonas@joonashakkinen.fi>
2019-02-07 18:37:06 +0200

    Add notification system

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import alertReducer from './reducers/alertReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const reducers = combineReducers({
+  alert: alertReducer,
+});
+const store = createStore(reducers);
+store.subscribe(() => localStorage.setItem('token', store.getState().user.token));
+export default store;
+
+ReactDOM.render((
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+), document.getElementById('root'));
